931ff6c32b008a206f310ad34dde40409641aad7
Your Name <your_email@example.com>
2026-06-10 22:33:11 +0530

    Fix logger configuration

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -2,10 +2,10 @@ import logging
 import os
 from datetime import datetime
 
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
 LOG_DIR = os.path.join(os.getcwd(), "logs")
 os.makedirs(LOG_DIR, exist_ok=True)
-
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
